7f04cdb9bd889a06cb66706d394b48f2435af959
Vincent REY <vrey@e2r11p13.42.fr>
2015-02-11 13:08:38 +0100

    RM(Useless dir): Removed a dir I cp\'ed instead of mv\'ing

--- macosx/Main.cpp
+++ macosx/Main.cpp
@@ -7,7 +7,13 @@ int main(int argc, char* argv[])
 	theWorld.Initialize(1024, 768, "Test", false, false, false);
 	theWorld.Add(new GridActor(), -1);
 	theWorld.StartGame();
-
+	Actor *a = new Actor();
+	a->SetPosition(-3.0f, 4.0f);
+	a->SetColor(1.0f, 0.0f, 0.0f);
+	a->SetDrawShape(ADS_Circle);
+	a->SetSize(3.0f);
+	theWorld.Add(a);
 	theWorld.Destroy();
+	a->Render();
 	return 0;
 }
