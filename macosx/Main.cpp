18e25b197cc82c228d8d31257d87cd9f5d9ff1e5
N0ich <vin100.rey@gmail.com>
2015-02-13 16:59:19 +0100

    Fix(Main.cpp): Fixes on the collisions problems Ground does not clutter anymore (ignored some of the redudant collisions) Walls behave strangely - if bottom angle hit - ground, if top angle hit - wall

--- macosx/Main.cpp
+++ macosx/Main.cpp
@@ -1,6 +1,16 @@
 #include <list>
 #include "stdafx.h"
 
+class HeroActor: public PhysicsActor {
+public:
+	virtual const String	GetClassName() {
+		return "HeroActor";
+	}
+	int				touchingGround;
+	int				touchingWall;
+	int				jumping;
+};
+
 class MouseDebugger: public MouseListener {
 public:
 	MouseDebugger(void) {
@@ -14,30 +24,125 @@ public:
 	};
 };
 
+/*
+** Very Important Class: Checks if contact with ground or wall, disabling it if necessary to prevent the clutter
+*/
+
+class rContactListener: public b2ContactListener {
+public:
+	void	BeginContact(b2Contact *contact) {
+		HeroActor*		hero = static_cast<HeroActor*>(Actor::GetNamed("Hero"));
+		ActorSet		actor = theTagList.GetObjectsTagged("actor");
+
+		if (hero->GetBody() == contact->GetFixtureA()->GetBody()) {
+			std::cout << "------------ BEGIN -------------" << std::endl;
+			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
+			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
+			for (ActorSet::iterator it = actor.begin(); it != actor.end(); it++) {
+				if (static_cast<PhysicsActor*>(*it)->GetBody() == contact->GetFixtureB()->GetBody()) {
+					//Touching ground
+					if (hero->GetBody()->GetWorldCenter().y > contact->GetFixtureB()->GetBody()->GetWorldCenter().y + 1 &&
+						(hero->GetBody()->GetWorldCenter().x > contact->GetFixtureB()->GetBody()->GetWorldCenter().x + 1.05 ||
+						 hero->GetBody()->GetWorldCenter().x > contact->GetFixtureB()->GetBody()->GetWorldCenter().x - 1.05)) {
+						hero->touchingGround++;
+						hero->jumping = 2;
+						if (hero->touchingGround > 1) {
+							contact->SetEnabled(false);
+						}
+						std::cout << "Ground Touched" << ": " << hero->touchingGround << std::endl;
+					}
+					//Touching Wall
+					else if (hero->GetBody()->GetWorldCenter().y < contact->GetFixtureB()->GetBody()->GetWorldCenter().y + 1) {
+						hero->touchingWall++;
+//						if (hero->touchingWall > 1) {
+//							contact->SetEnabled(false);
+//						}
+						std::cout << "Wall Touched" << ": " << hero->touchingWall << std::endl;
+					}
+					else
+						std::cout << "Contact ignored" << std::endl;
+				}
+			}
+			std::cout << "------------- /BEGIN --------- " << std::endl;
+		}
+		else if  (hero->GetBody() == contact->GetFixtureB()->GetBody())
+			std::cout << "Hero in B" << std::endl;
+	};
+
+	void	EndContact(b2Contact *contact) {
+		HeroActor	*hero = static_cast<HeroActor*>(Actor::GetNamed("Hero"));
+		ActorSet	actor = theTagList.GetObjectsTagged("actor");
+
+		if (hero->GetBody() == contact->GetFixtureA()->GetBody()) {
+			std::cout << "------------ END -------------" << std::endl;
+			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
+			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
+			for (ActorSet::iterator it = actor.begin(); it != actor.end(); it++) {
+				if (static_cast<PhysicsActor*>(*it)->GetBody() == contact->GetFixtureB()->GetBody()) {
+					//if contactB == ground - TODO
+					if (hero->GetBody()->GetWorldCenter().y > contact->GetFixtureB()->GetBody()->GetWorldCenter().y + 1 &&
+						(hero->GetBody()->GetWorldCenter().x > contact->GetFixtureB()->GetBody()->GetWorldCenter().x + 1.05 ||
+						 hero->GetBody()->GetWorldCenter().x > contact->GetFixtureB()->GetBody()->GetWorldCenter().x - 1.05)) {
+						//Leaving the ground
+						std::cout << "Leaving ground" << std::endl;
+
+						hero->touchingGround--;
+						if (hero->touchingGround <= 0) {
+							hero->jumping--;
+							hero->touchingGround = 0;
+						}
+					}
+					else if (hero->touchingWall > 0) {
+						std::cout << "Leaving wall" << std::endl;
+						hero->touchingWall--;
+					}
+				}
+			}
+			std::cout << "-------------- /END ----------" << std::endl;
+		}
+	};
+};
+
+
+class GroundActor: public PhysicsActor {
+public:
+	virtual const String	GetClassName() {
+		return "GroundActor";
+	}
+	bool _isRightUsed;
+	bool _isLeftUsed;
+	bool _isTopUsed;
+	bool _isBotUsed;
+};
+
+
 class Hero: public GameManager {
 
 public:
 	Hero(void) {
-		this->p1 = new PhysicsActor();
+
+		this->p1 = new HeroActor();
 		this->p1->SetPosition(-3.0f, 5.0f);
 		this->p1->SetDrawShape(ADS_Square);
 		this->p1->SetSize(1.0f);
+		this->p1->SetColor(0,0,0);
 		this->p1->SetDensity(1.0f);
 		this->p1->SetFriction(1.0f);
 		this->p1->SetName("Hero");
 		this->p1->SetRestitution(0.0f);
-		this->p1->SetSprite("Resources/Images/kappa.png");
+//		this->p1->SetSprite("Resources/Images/kappa.png");
 		this->p1->SetShapeType(PhysicsActor::SHAPETYPE_BOX);
 		this->p1->InitPhysics();
 		this->p1->GetBody()->SetFixedRotation(true);
 		theWorld.Add(this->p1);
 
+
 		for (int i = -13; i <= 18; i++) {
-			this->floor.push_back(new PhysicsActor());
+			this->floor.push_back(new GroundActor());
 		}
 
 		int i = -13;
-		for (std::list<PhysicsActor*>::iterator it = this->floor.begin(); it != this->floor.end(); it++) {
+		for (std::list<GroundActor*>::iterator it = this->floor.begin(); it != this->floor.end(); it++) {
 			if (i > 13)
 				(*it)->SetPosition(5, 8 - i);
 			else
@@ -46,7 +151,7 @@ public:
 			(*it)->SetSize(1);
 			(*it)->SetDensity(0.0f);
 			(*it)->SetColor(1, 0, 0);
-			(*it)->Tag("Floor");
+			(*it)->Tag("Floor, actor");
 			(*it)->SetFriction(1.0f);
 			(*it)->SetRestitution(0.0f);
 			(*it)->SetShapeType(PhysicsActor::SHAPETYPE_BOX);
@@ -61,6 +166,7 @@ public:
 		theSwitchboard.SubscribeTo(this, "ForReleased");
 		theSwitchboard.SubscribeTo(this, "BackReleased");
 		theSwitchboard.SubscribeTo(this, "Smash");
+
 		theSwitchboard.SubscribeTo(this, "Attack");
 		theSwitchboard.SubscribeTo(this, "CollisionStartWithHero");
 		theSwitchboard.SubscribeTo(this, "CollisionEndWithHero");
@@ -70,53 +176,41 @@ public:
 	};
 
 	void	ReceiveMessage(Message *m) {
-		if (m->GetMessageName() == "Jump" && this->_jumping == 0) {
+		if (m->GetMessageName() == "Jump" && this->p1->jumping > 0) {
 			this->p1->ApplyForce(Vector2(0.0f, 500.0f), Vector2(0.0f, 0.0f));
-			this->_jumping = 1;
+			if (p1->touchingGround == 0)
+				p1->jumping--;
 		}
 
 		if (m->GetMessageName() == "Smash") {
 			this->p1->ApplyForce(Vector2(0.0f, -10000.0f), Vector2(0.0f, 0.0f));
 		}
 
-		if (m->GetMessageName() == "ForPressed") {
+		if (m->GetMessageName() == "ForPressed" || m->GetMessageName() == "ForReleased") {
 			MoveForward();
 		}
-		if (m->GetMessageName() == "BackPressed") {
+		if (m->GetMessageName() == "BackPressed" || m->GetMessageName() == "BackReleased") {
 			MoveBackward();
-		}
-		if (m->GetMessageName() == "ForReleased") {
-			MoveForward();
-		}
-		if (m->GetMessageName() == "BackReleased") {
-			MoveBackward();
-		}
-
-		if (m->GetMessageName() == "CollisionStartWithHero") {
-			std::cout << "Collision" << std::endl;
-			this->_jumping = 0;
 		}
 	};
 
  	void	MoveForward(void) {
  		if (this->p1->GetBody()->GetLinearVelocity().x < 5) {
-			std::cout << this->p1->GetBody()->GetLinearVelocity().x << std::endl;
- 			this->p1->ApplyForce(Vector2(200.0f, 0.0f), Vector2(0.0f, 0.0f));
+ 			this->p1->ApplyForce(Vector2(50.0f, 0.0f), Vector2(0.0f, 0.0f));
 		}
  	};
 
  	void	MoveBackward(void) {
  		if (this->p1->GetBody()->GetLinearVelocity().x > -5) {
-			std::cout << this->p1->GetBody()->GetLinearVelocity().x << std::endl;
- 			this->p1->ApplyForce(Vector2(-200.0f, 0.0f), Vector2(0.0f, 0.0f));
+ 			this->p1->ApplyForce(Vector2(-50.0f, 0.0f), Vector2(0.0f, 0.0f));
 		}
  	};
 
 private:
-	std::list<PhysicsActor*> floor;
-	PhysicsActor	*p1;
+	std::list<GroundActor*> floor;
+	HeroActor		*p1;
+	PhysicsActor	*map;
 
-	int				_jumping;
 };
 
 int main(int argc, char* argv[])
@@ -124,12 +218,15 @@ int main(int argc, char* argv[])
 	theWorld.Initialize(1920, 1024, "Test", false, false, false);
 	theWorld.Add(new GridActor(), -1);
 
-
-
 	theWorld.SetupPhysics();
 	theWorld.SetSideBlockers(true, 0.7f);
 
+	rContactListener cListen;
+	theWorld.GetPhysicsWorld().SetContactListener(&cListen);
+
 	Hero salut;
+
+
 
 	b2Vec2 v1(-14, -9.6f);
 	b2Vec2 v2(4, -9.6f);
