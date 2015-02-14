e551c58676c88f692255872ea70356b5cc431edb
N0ich <vin100.rey@gmail.com>
2015-02-14 15:42:49 +0100

    Add(Main.cpp): Working on the hit/take hit - works as intended, almost

--- macosx/Main.cpp
+++ macosx/Main.cpp
@@ -10,6 +10,9 @@ public:
 	int					touchingWall;
 	int					jumping;
 	std::list<b2Body*>	grounds;
+	int					orientation;
+	int					up;
+	bool				canMove;
 };
 
 class MouseDebugger: public MouseListener {
@@ -33,55 +36,81 @@ class rContactListener: public b2ContactListener {
 public:
 	void	BeginContact(b2Contact *contact) {
 		HeroActor*		hero = static_cast<HeroActor*>(Actor::GetNamed("Hero"));
+		PhysicsActor*	weapon = static_cast<PhysicsActor*>(Actor::GetNamed("Weapon"));
 		ActorSet		actor = theTagList.GetObjectsTagged("physics");
 
 		if (hero->GetBody() == contact->GetFixtureA()->GetBody()) {
-			std::cout << "------------ BEGIN -------------" << std::endl;
-			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
-			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
+// 			std::cout << "------------ BEGIN -------------" << std::endl;
+// 			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
+// 			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
 			for (ActorSet::iterator it = actor.begin(); it != actor.end(); it++) {
 				if (static_cast<PhysicsActor*>(*it)->GetBody() == contact->GetFixtureB()->GetBody()) {
-					//Touching ground
-					if (hero->GetBody()->GetWorldCenter().y > contact->GetFixtureB()->GetBody()->GetWorldCenter().y + 1) {
-						hero->jumping = 2;
-						int		found = 0;
-						if (hero->grounds.size() > 0)
-							contact->SetEnabled(false);
-						for (std::list<b2Body*>::iterator it = hero->grounds.begin(); it != hero->grounds.end(); it++) {
-							if ((*it) == contact->GetFixtureB()->GetBody()) {
-								found = 1;
+					//Case ground/wall
+					if ((*it)->IsTagged("ground") == true) {
+						if (hero->GetBody()->GetWorldCenter().y > contact->GetFixtureB()->GetBody()->GetWorldCenter().y + 1) {
+							hero->jumping = 2;
+							int		found = 0;
+							if (hero->grounds.size() > 0)
 								contact->SetEnabled(false);
+							for (std::list<b2Body*>::iterator it = hero->grounds.begin(); it != hero->grounds.end(); it++) {
+								if ((*it) == contact->GetFixtureB()->GetBody()) {
+									found = 1;
+									contact->SetEnabled(false);
+								}
+							}
+							if (found == 0) {
+								hero->grounds.push_back(contact->GetFixtureB()->GetBody());
+//							std::cout << "Ground Touched" << hero->grounds.size() << std::endl;
 							}
 						}
-						if (found == 0) {
-							hero->grounds.push_back(contact->GetFixtureB()->GetBody());
-							std::cout << "Ground Touched" << hero->grounds.size() << std::endl;
+						//Touching Wall
+						else {
+							hero->touchingWall++;
+							if (hero->touchingWall > 1) {
+								contact->SetEnabled(false);
+							}
+//						std::cout << "Wall Touched" << hero->touchingWall << std::endl;
 						}
 					}
-					//Touching Wall
-					else {
-						hero->touchingWall++;
-						if (hero->touchingWall > 1) {
-							contact->SetEnabled(false);
-						}
-						std::cout << "Wall Touched" << hero->touchingWall << std::endl;
+					//Case ennemy
+					else if ((*it)->IsTagged("victim")) {
+						hero->canMove = 0;
+						theSwitchboard.DeferredBroadcast(new Message("CanMove"), 1.5f);
+						hero->GetBody()->SetLinearVelocity(b2Vec2(0, 0));
+						if (static_cast<PhysicsActor*>((*it))->GetBody()->GetWorldCenter().x > hero->GetBody()->GetWorldCenter().x)
+							hero->ApplyLinearImpulse(Vector2(-20, 20), Vector2(0, 0));
+						else
+							hero->ApplyLinearImpulse(Vector2(20, 20), Vector2(0, 0));
 					}
 				}
+//			std::cout << "------------- /BEGIN --------- " << std::endl;
 			}
-			std::cout << "------------- /BEGIN --------- " << std::endl;
 		}
-		else if  (hero->GetBody() == contact->GetFixtureB()->GetBody())
-			std::cout << "Hero in B" << std::endl;
+		else if  (hero->GetBody() == contact->GetFixtureB()->GetBody()) {
+//			std::cout << "Hero in B" << std::endl;
+		}
+		else if (weapon) {
+			if (weapon->GetBody() == contact->GetFixtureB()->GetBody())
+				WeaponCollision(contact->GetFixtureA()->GetBody(), weapon);
+			else if (weapon->GetBody() == contact->GetFixtureA()->GetBody())
+				WeaponCollision(contact->GetFixtureB()->GetBody(), weapon);
+		}
 	};
+	void	WeaponCollision(b2Body *contact, PhysicsActor* weapon) {
+		if (weapon->GetBody()->GetWorldCenter().x > contact->GetWorldCenter().x)
+			contact->ApplyLinearImpulse(b2Vec2(-20, 20), contact->GetWorldCenter());
+		if (weapon->GetBody()->GetWorldCenter().x < contact->GetWorldCenter().x)
+			contact->ApplyLinearImpulse(b2Vec2(20, 20), contact->GetWorldCenter());
+	}
 
 	void	EndContact(b2Contact *contact) {
 		HeroActor	*hero = static_cast<HeroActor*>(Actor::GetNamed("Hero"));
 		ActorSet	actor = theTagList.GetObjectsTagged("physics");
 
 		if (hero->GetBody() == contact->GetFixtureA()->GetBody()) {
-			std::cout << "------------ END -------------" << std::endl;
-			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
-			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
+// 			std::cout << "------------ END -------------" << std::endl;
+// 			std::cout << "X: " << hero->GetBody()->GetWorldCenter().x << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().x << std::endl;
+// 			std::cout << "Y: " << hero->GetBody()->GetWorldCenter().y << ":" << contact->GetFixtureB()->GetBody()->GetWorldCenter().y << std::endl;
 			for (ActorSet::iterator it = actor.begin(); it != actor.end(); it++) {
 				if (static_cast<PhysicsActor*>(*it)->GetBody() == contact->GetFixtureB()->GetBody()) {
 					//if contactB == ground - TODO
@@ -91,15 +120,15 @@ public:
 						if (hero->grounds.size() == 0) {
 							hero->jumping--;
 						}
-						std::cout << "Leaving ground: " << hero->grounds.size() << std::endl;
+//						std::cout << "Leaving ground: " << hero->grounds.size() << std::endl;
 					}
 					else if (hero->touchingWall > 0) {
 						hero->touchingWall--;
-						std::cout << "Leaving wall:" << hero->touchingWall << std::endl;
+//						std::cout << "Leaving wall:" << hero->touchingWall << std::endl;
 					}
 				}
 			}
-			std::cout << "-------------- /END ----------" << std::endl;
+//			std::cout << "-------------- /END ----------" << std::endl;
 		}
 	};
 };
@@ -110,10 +139,6 @@ public:
 	virtual const String	GetClassName() {
 		return "GroundActor";
 	}
-	bool _isRightUsed;
-	bool _isLeftUsed;
-	bool _isTopUsed;
-	bool _isBotUsed;
 };
 
 
@@ -123,8 +148,9 @@ public:
 	Hero(void) {
 
 		this->p1 = new HeroActor();
-		this->p1->SetPosition(-3.0f, 5.0f);
+ 		this->p1->SetPosition(-3.0f, 5.0f);
 		this->p1->SetColor(0,0,0);
+		this->p1->orientation = 1;
 		this->p1->SetSize(1);
 		this->p1->SetDrawShape(ADS_Square);
 		this->p1->SetName("Hero");
@@ -133,13 +159,30 @@ public:
 		this->p1->SetFriction(1.0f);
 		this->p1->SetRestitution(0.0f);
 		this->p1->SetFixedRotation(true);
+		this->p1->canMove = true;
 		this->p1->Tag("hero");
 
 		this->p1->InitPhysics();
 		theWorld.Add(this->p1);
 
+		this->victim = new PhysicsActor();
+ 		this->victim->SetPosition(7.0f, 5.0f);
+		this->victim->SetColor(0,1,0);
+		this->victim->SetSize(1);
+		this->victim->SetDrawShape(ADS_Square);
+		this->victim->SetName("Victim");
+		this->victim->SetShapeType(PhysicsActor::SHAPETYPE_BOX);
+		this->victim->SetDensity(1.0f);
+		this->victim->SetFriction(1.0f);
+		this->victim->SetRestitution(0.0f);
+		this->victim->SetFixedRotation(true);
+		this->victim->Tag("victim, physics");
 
-		for (int i = -13; i <= 18; i++) {
+		this->victim->InitPhysics();
+		theWorld.Add(this->victim);
+
+
+		for (int i = -13; i <= 13; i++) {
 			this->floor.push_back(new GroundActor());
 		}
 
@@ -155,6 +198,7 @@ public:
 			(*it)->SetColor(1, 0, 0);
 			(*it)->Tag("ground, Physics");
 			(*it)->SetFriction(1.0f);
+			(*it)->SetName("Ground");
 			(*it)->SetRestitution(0.0f);
 			(*it)->SetShapeType(PhysicsActor::SHAPETYPE_BOX);
 			(*it)->InitPhysics();
@@ -165,13 +209,21 @@ public:
 		theSwitchboard.SubscribeTo(this, "Jump");
 		theSwitchboard.SubscribeTo(this, "ForPressed");
 		theSwitchboard.SubscribeTo(this, "BackPressed");
+		theSwitchboard.SubscribeTo(this, "UpPressed");
+		theSwitchboard.SubscribeTo(this, "DownPressed");
+		theSwitchboard.SubscribeTo(this, "DownReleased");
+		theSwitchboard.SubscribeTo(this, "UpReleased");
 		theSwitchboard.SubscribeTo(this, "ForReleased");
 		theSwitchboard.SubscribeTo(this, "BackReleased");
 		theSwitchboard.SubscribeTo(this, "Smash");
-
 		theSwitchboard.SubscribeTo(this, "Attack");
+		theSwitchboard.SubscribeTo(this, "CanMove");
 		theSwitchboard.SubscribeTo(this, "CollisionStartWithHero");
 		theSwitchboard.SubscribeTo(this, "CollisionEndWithHero");
+		theSwitchboard.SubscribeTo(this, "CollisionStartWithWeapon");
+		theSwitchboard.SubscribeTo(this, "CollisionStartWithVictim");
+		theSwitchboard.SubscribeTo(this, "CollisionStart:Hero.Victim");
+		theSwitchboard.SubscribeTo(this, "DeleteWeapon");
 	};
 
 	~Hero(void) {
@@ -179,40 +231,91 @@ public:
 
 	void	ReceiveMessage(Message *m) {
 		if (m->GetMessageName() == "Jump" && this->p1->jumping > 0) {
-			this->p1->ApplyLinearImpulse(Vector2(0.0f, 30.0f), Vector2(0.0f, 0.0f));
-			if (p1->grounds.size() == 0)
-				p1->jumping--;
+			if (this->p1->canMove == 1) {
+				this->p1->ApplyLinearImpulse(Vector2(0.0f, 30.0f), Vector2(0.0f, 0.0f));
+				if (p1->grounds.size() == 0)
+					p1->jumping--;
+			}
 		}
 
 		if (m->GetMessageName() == "Smash") {
-			this->p1->ApplyLinearImpulse(Vector2(0.0f, -1000.0f), Vector2(0.0f, 0.0f));
+			if (this->p1->canMove == 1)
+				this->p1->ApplyLinearImpulse(Vector2(0.0f, -1000.0f), Vector2(0.0f, 0.0f));
 		}
 
-		if (m->GetMessageName() == "ForPressed" || m->GetMessageName() == "ForReleased") {
-			MoveForward();
+		if (m->GetMessageName() == "Attack") {
+			this->weapon = new PhysicsActor();
+			this->weapon->SetPosition(this->p1->GetBody()->GetWorldCenter().x + this->p1->orientation, this->p1->GetBody()->GetWorldCenter().y +this->p1->up);
+			this->weapon->SetColor(0,0,1);
+			this->weapon->SetSize(1);
+			this->weapon->SetDrawShape(ADS_Square);
+			this->weapon->SetName("Weapon");
+			this->weapon->SetShapeType(PhysicsActor::SHAPETYPE_BOX);
+			this->weapon->SetDensity(0.0f);
+			this->weapon->SetFriction(1.0f);
+			this->weapon->SetRestitution(0.3f);
+			this->weapon->SetFixedRotation(true);
+			this->weapon->Tag("weapon");
+			this->weapon->SetIsSensor(true);
+			this->weapon->InitPhysics();
+			theWorld.Add(this->weapon);
 		}
-		if (m->GetMessageName() == "BackPressed" || m->GetMessageName() == "BackReleased") {
-			MoveBackward();
+
+		if (m->GetMessageName() == "DeleteWeapon") {
+			if (this->weapon) {
+				theWorld.Remove(this->weapon);
+				delete this->weapon;
+			}
 		}
+
+		if (m->GetMessageName() == "CollisionStart:Hero.Ground") {
+		}
+
+		else if (m->GetMessageName() == "ForPressed" || m->GetMessageName() == "ForReleased") {
+			this->p1->orientation = 1;
+			this->p1->up = 0;
+			if (this->p1->canMove == true) {
+				MoveForward();
+			}
+		}
+		else if (m->GetMessageName() == "BackPressed" || m->GetMessageName() == "BackReleased") {
+			this->p1->orientation = -1;
+			this->p1->up = 0;
+			if (this->p1->canMove == true) {
+				MoveBackward();
+			}
+		}
+		else if (m->GetMessageName() == "UpPressed" || m->GetMessageName() == "UpReleased") {
+			this->p1->orientation = 0;
+			this->p1->up = 1;
+		}
+		else if (m->GetMessageName() == "DownPressed" || m->GetMessageName() == "DownReleased") {
+			this->p1->orientation = 0;
+			this->p1->up = -1;
+		}
+
+		else if (m->GetMessageName() == "CanMove")
+			this->p1->canMove = 1;
 	};
 
  	void	MoveForward(void) {
- 		if (this->p1->GetBody()->GetLinearVelocity().x < 5) {
- 			this->p1->ApplyLinearImpulse(Vector2(8.0f, 0.0f), Vector2(0.0f, 0.0f));
+ 		if (this->p1->GetBody()->GetLinearVelocity().x < 10) {
+ 			this->p1->ApplyLinearImpulse(Vector2(10.0f, 0.0f), Vector2(0.0f, 0.0f));
 		}
  	};
 
  	void	MoveBackward(void) {
- 		if (this->p1->GetBody()->GetLinearVelocity().x > -5) {
- 			this->p1->ApplyLinearImpulse(Vector2(-8.0f, 0.0f), Vector2(0.0f, 0.0f));
+ 		if (this->p1->GetBody()->GetLinearVelocity().x > -10) {
+ 			this->p1->ApplyLinearImpulse(Vector2(-10.0f, 0.0f), Vector2(0.0f, 0.0f));
 		}
  	};
 
 private:
 	std::list<GroundActor*> floor;
 	HeroActor		*p1;
+	PhysicsActor	*weapon;
 	PhysicsActor	*map;
-
+	PhysicsActor	*victim;
 };
 
 int main(int argc, char* argv[])
